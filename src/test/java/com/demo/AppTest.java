7a659d83b69d274bc28b1532e28d323f4b496728
Ritish145 <87793001+Ritish145@users.noreply.github.com>
2026-09-21 16:18:31 -0400

    Update AppTest.java

--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AppTest {
     @Test
     void addsTwoNumbers() {
-        assertEquals(6, App.add(2, 3));
+        assertEquals(5, App.add(2, 3));
     }
 }
